f9a9c24bf1d7357e6a5c9c13f26646fdd68c7d68
songjaeheon <spes2you@gmail.com>
2024-03-13 11:41:19 +0900

    feat: Spring Initializr

---
+++ src/main/java/com/footballmania/waspayment/WasPaymentApplication.java
@@ -0,0 +1,13 @@
+package com.footballmania.waspayment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WasPaymentApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WasPaymentApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/footballmania/waspayment/WasPaymentApplicationTests.java
@@ -0,0 +1,13 @@
+package com.footballmania.waspayment;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WasPaymentApplicationTests {
+
+	// @Test
+	// void contextLoads() {
+	// }
+
+}
